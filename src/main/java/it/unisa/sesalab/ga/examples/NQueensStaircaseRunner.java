5f1edb46236cbc6bd947ddb471d2ce3053e0fd86
emaiannone <emaiannone96@gmail.com>
2021-10-27 17:44:30 +0200

    Irrelevant changes

--- src/main/java/it/unisa/sesalab/ga/examples/NQueensStaircaseRunner.java
+++ src/main/java/it/unisa/sesalab/ga/examples/NQueensStaircaseRunner.java
@@ -4,7 +4,6 @@ import it.unisa.sesalab.ga.examples.fix.IntegerNPointCrossover;
 import it.unisa.sesalab.ga.examples.problem.NQueensStaircaseProblem;
 import org.uma.jmetal.algorithm.multiobjective.nsgaii.NSGAII;
 import org.uma.jmetal.algorithm.multiobjective.nsgaii.NSGAIIBuilder;
-import org.uma.jmetal.component.ranking.impl.FastNonDominatedSortRanking;
 import org.uma.jmetal.example.AlgorithmRunner;
 import org.uma.jmetal.operator.crossover.CrossoverOperator;
 import org.uma.jmetal.operator.mutation.impl.IntegerPolynomialMutation;
@@ -14,19 +13,18 @@ import org.uma.jmetal.solution.integersolution.IntegerSolution;
 import org.uma.jmetal.util.JMetalLogger;
 import org.uma.jmetal.util.comparator.RankingAndCrowdingDistanceComparator;
 
-import java.util.Arrays;
 import java.util.List;
 
 public class NQueensStaircaseRunner {
     public static void main(String[] args) {
-        int chessSize = 8;
+        int chessSize = 20;
 
         double crossoverProbability = 0.8;
         double mutationProbability = 0.01;
         int maxEvaluations = 1000000;
         int populationSize = 20;
 
-        Problem<IntegerSolution> problem = new NQueensStaircaseProblem("EightQueensStaircase", chessSize);
+        Problem<IntegerSolution> problem = new NQueensStaircaseProblem("TwentyQueensStaircase", chessSize);
         BinaryTournamentSelection<IntegerSolution> selection = new BinaryTournamentSelection<>(new RankingAndCrowdingDistanceComparator<>());
         CrossoverOperator<IntegerSolution> crossover = new IntegerNPointCrossover(crossoverProbability, 1);
         IntegerPolynomialMutation mutation = new IntegerPolynomialMutation(mutationProbability, 0);
@@ -41,6 +39,7 @@ public class NQueensStaircaseRunner {
 
         JMetalLogger.logger.info(String.format("Problem: %s", problem.getName()));
         JMetalLogger.logger.info(String.format("Solutions: \n%s\n", bestIndividuals));
+        // TODO Do not return identical solutions
         JMetalLogger.logger.info(String.format("Total execution time: %s ms", nsgaiiRunner.getComputingTime()));
     }
 }
